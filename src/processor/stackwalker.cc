de33482a9d3b28cd0013813ce0e39c57eb79b0ee
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-05-28 15:44:51 +0200

    fix: Check for null-ref of thread_memory

--- src/processor/stackwalker.cc
+++ src/processor/stackwalker.cc
@@ -202,7 +202,7 @@ MemoryRegion *GetActualStackMemory(
   }
 
   uint64_t ignored;
-  if (thread_memory->GetMemoryAtAddress(stack_pointer, &ignored)) {
+  if (thread_memory && thread_memory->GetMemoryAtAddress(stack_pointer, &ignored)) {
     return thread_memory;
   }
 
